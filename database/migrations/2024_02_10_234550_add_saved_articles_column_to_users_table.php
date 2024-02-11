84ecffde98945196c0e7f1cdf31f558074903f23
Zeinab <zeinabfarhat38@gmail.com>
2024-02-11 22:03:05 +0200

    Fixed the migration add_saved_article_column

--- database/migrations/2024_02_10_234550_add_saved_articles_column_to_users_table.php
+++ database/migrations/2024_02_10_234550_add_saved_articles_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('saved_articles')->nullable();
+            $table->json('saved_articles')->nullable()->after('password');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('saved_articles')->after('password');
+            $table->dropColumn('saved_articles');
         });
     }
 };
